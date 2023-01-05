9db58bcee8dafe05078219f525f08d440687ddab
Jerrica Bobadilla <jbobadilla27@gmail.com>
2023-01-05 09:53:28 -0800

    rename tests folder

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,7 +6,7 @@ import {
   waitForElementToBeRemoved,
 } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import { server } from "./mocks/server";
+import { server } from "../mocks/server";
 
 import App from "../components/App";
 
